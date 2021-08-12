44698d5dbc05a27be2f9c605dbbf628985d9cfff
Krishi Sharma <krishi.sharma@kungfu.ai>
2021-08-11 21:12:53 -0500

    reorganize app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {DatabaseModule} from "./db/database.module";
+import {DatabaseModule} from "../db/database.module";
 
 @Module({
   imports: [DatabaseModule],
